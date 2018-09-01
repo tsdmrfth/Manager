f9ddcecb49ece78df9c46316f4e7e0fe11d8237c
Fatih Tasdemir <tsdmrfth@gmail.com>
2018-09-02 02:29:51 +0300

    Added Firebase dependency and configuration.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducers from './reducers';
+import firebase from 'firebase';
+import {name} from 'app.json';
 
 /**
  * Created by Fatih Taşdemir on 2.09.2018
  */
 
 class App extends Component {
+
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyBP6eTxxdTVpWjR7FbuNgUeiO0x1jHlFdg',
+            authDomain: 'manager-d8d87.firebaseapp.com',
+            databaseURL: 'https://manager-d8d87.firebaseio.com',
+            projectId: 'manager-d8d87',
+            storageBucket: 'manager-d8d87.appspot.com',
+            messagingSenderId: '563445212087'
+        };
+        firebase.initializeApp(config, name);
+    }
 
     render() {
         return (
