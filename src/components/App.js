6cc86734ed5fa5f3ca93de356102a4c75f8beb17
Fatih Tasdemir <tsdmrfth@gmail.com>
2018-09-02 01:53:49 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View} from 'react-native';
+
+/**
+ * Created by Fatih Taşdemir on 2.09.2018
+ */
+
+const App = () => {
+
+    return (
+        <View/>
+    );
+
+};
+
+export default App;
