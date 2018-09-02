f9691a874305a9d3ac15612ca2181f9907287716
Fatih Tasdemir <tsdmrfth@gmail.com>
2018-09-02 04:17:59 +0300

    Created action creators and reducers and connected to LoginScreen.

--- src/ui/LoginScreen.js
+++ src/ui/LoginScreen.js
@@ -1,29 +1,45 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import {Button, CardSection, Input} from "../components/common";
+import {connect} from 'react-redux';
+import {emailChanged, passwordChanged} from '../actions';
+import Reactotron from "reactotron-react-native";
 
 /**
  * Created by Fatih Taşdemir on 2.09.2018
  */
 
-export default class LoginScreen extends Component {
+class LoginScreen extends Component {
+
+    onEmailChanged(text) {
+        this.props.emailChanged(text)
+    }
+
+    onPasswordChanged(text) {
+        this.props.passwordChanged(text);
+    }
 
     render() {
+
+        const {email} = this.props;
 
         return (
             <View>
 
                 <CardSection>
                     <Input
+                        value={email}
                         label={'Email'}
-                        placeholder={'mail@email.com'}/>
+                        placeholder={'mail@email.com'}
+                        whenTextChanged={this.onEmailChanged.bind(this)}/>
                 </CardSection>
 
                 <CardSection>
                     <Input
                         label={'Password'}
                         placeholder={'************'}
-                        isPassword/>
+                        isPassword
+                        whenTextChanged={this.onPasswordChanged.bind(this)}/>
                 </CardSection>
 
                 <CardSection>
@@ -36,3 +52,13 @@ export default class LoginScreen extends Component {
     }
 
 }
+
+const mapStateToProps = state => {
+    Reactotron.log(state.auth);
+    return {
+        email: state.auth.email,
+        password: state.auth.password
+    }
+};
+
+export default connect(mapStateToProps, {emailChanged, passwordChanged})(LoginScreen);
